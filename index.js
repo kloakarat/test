898dbfa71234a42968b53bc156719df744b5f84c
K.Akarat's <akarat.klo@gmail.com>
2024-01-21 20:15:17 +0700

    no message

--- index.js
+++ index.js
@@ -1,15 +1,47 @@
 const express = require("express");
 const app = express();
 const bodyParser = require("body-parser");
-
-app.use(bodyParser.json());
+const htmlToPdf = require("html-pdf-node");
+const ejs = require("ejs");
 
 const port = 1000;
+
+app.use(bodyParser.json());
 
 app.get('/', (req, res) => {
   res.send('Hello World')
 })
 
+app.post("/pdf", async function (req, res) {
+  const tableHtml = await ejs.renderFile(
+    "./templates/table.html.ejs",
+    { rows: req.body.rows },
+    { async: true }
+  );
+  const html = await ejs.renderFile(
+    "./templates/layout.html.ejs",
+    { body: tableHtml },
+    { async: true }
+  );
+  let options = {
+    format: "A4",
+    margin: { top: 15, left: 10, right: 10, bottom: 15 },
+  };
+  let file = { content: html };
+  htmlToPdf
+    .generatePdf(file, options)
+    .then((pdfBuffer) => {
+      res
+        .writeHead(200, {
+          "Content-Type": "application/pdf",
+          "Content-Disposition": "attachment",
+        })
+        .end(pdfBuffer);
+    })
+    .catch((err) => {
+      res.send({ success: false, error: err });
+    });
+});
 
 app.listen(port, () => {
   return console.log(
